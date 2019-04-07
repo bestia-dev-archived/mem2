88211deb10fa635a93eb6cd610d5dc945081a0e0
Luciano Bestia <luciano.bestia@gmail.com>
2019-04-07 18:38:18 +0700

    commenting

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 // An attribute to hide warnings for unused code.
 //#![allow(dead_code)]
+
 //region: use statements
 use dodrio::bumpalo::{self, Bump};
 use dodrio::{Node, Render};
@@ -10,6 +11,7 @@ use wasm_bindgen::JsCast;
 use rand::rngs::OsRng;
 use rand::Rng;
 //endregion
+
 //region: enum, structs, const,...
 const GAME_TITLE: &'static str = "mem2";
 const SRC_FOR_CARD_FACE_DOWN: &'static str = "content/img/mem_image_00_cardfacedown.png";
@@ -25,7 +27,7 @@ For the sake of simplicity, it is made as for single player mode. \
 The simple memory game is for kids. The images are funny cartoon characters from the alphabet. \
 The cards grid is only 4x4.";
 
-//the zero element is card-facedown or empty, alphabet begins with 01-A
+//the zero element is card_facedown or empty, alphabet begins with 01 : A
 const SPELLING: [&'static str; 27] = [
     "", "alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel", "india",
     "juliet", "kilo", "lima", "mike", "november", "oscar", "papa", "quebec", "romeo", "sierra",
@@ -40,9 +42,9 @@ enum CardStatus {
 
 struct Card {
     status: CardStatus,
-    //src attribute for HTML element image, filename of card image
+    //field for src attribute for HTML element imagea and filename of card image
     card_number_and_img_src: usize,
-    //id attribute for HTML element image contains the card index
+    //field for id attribute for HTML element image contains the card index
     card_index_and_id: usize,
 }
 
@@ -51,17 +53,16 @@ struct CardGrid {
     //The player in one turn clicks 2 times and open 2 cards. If not match,
     //the third click closes opened cards and
     //it starts the next player turn.
-    count_click_inside_one_turn: i32,
+    count_click_inside_one_turn: u32,
     card_index_of_first_click: usize,
     card_index_of_second_click: usize,
     //counts only clicks that flip the card. The third click is not counted.
-    count_all_clicks: i32,
+    count_all_clicks: u32,
 }
-
 //endregion
 
 impl CardGrid {
-    /// Construct a new `CardGrid` component.
+    /// Construct a new `CardGrid` component. Only once on the begining.
     fn new() -> CardGrid {
         //region: find 8 distinct random numbers between 1 and 26 for the alphabet cards
         //vec_of_random_numbers is 0 based
@@ -95,16 +96,16 @@ impl CardGrid {
         dbg!("vec_of_random_numbers values");
         let mut vec_card_from_random_numbers = Vec::new();
 
-        //region: Index 0 is reserved for FaceDown. Cards start with base 1
+        //region: Index 0 is special and reserved for FaceDown. Cards start with base 1
         let new_card = Card {
             status: CardStatus::CardFaceDown,
-            //dereference random number from iterator
             card_number_and_img_src: 0,
-            //card base index will be 1. 0 is reserved for FaceDown.
             card_index_and_id: 0,
         };
         vec_card_from_random_numbers.push(new_card);
+        //endregion
 
+        //region: create the 16 card and push to the vector
         for (index, random_number) in vec_of_random_numbers.iter().enumerate() {
             let new_card = Card {
                 status: CardStatus::CardFaceDown,
@@ -117,6 +118,7 @@ impl CardGrid {
         }
         //endregion
         //endregion
+
         //region: return from constructor
         CardGrid {
             vec_cards: vec_card_from_random_numbers,
@@ -129,8 +131,10 @@ impl CardGrid {
     }
 }
 
-//region: The `Render` implementation.
+//region: `Render` implementation.
 //It is called for every Dodrio animation frame to render the vdom.
+//Probably only when something changes. Here it is a click on the cards.
+//Not sure about that, but I don't see a reason to make execute it otherwise.
 impl Render for CardGrid {
     fn render<'a, 'bump>(&'a self, bump: &'bump Bump) -> Node<'bump>
     where
@@ -149,8 +153,8 @@ impl Render for CardGrid {
         };
 
         //The on_click event passed by javascript executes all the logic
-        //to change the fields of the CardGrid struct.
-        //That stuct is the only source of data to later render the virtual dom.
+        //and changes only the fields of the CardGrid struct.
+        //That stuct is the only permanent data storage for later render the virtual dom.
         let closure_on_click = |card_grid: &mut CardGrid, img: web_sys::HtmlImageElement| {
             //we have 3 possible clicks in one turn with different code branches.
             if card_grid.count_click_inside_one_turn >= 2 {
@@ -172,15 +176,16 @@ impl Render for CardGrid {
                         card_grid.vec_cards[this_click_card_index].status =
                             CardStatus::CardFaceUpTemporary;
                         if card_grid.count_click_inside_one_turn == 0 {
-                            //before the first click reset the spelling. Usefull when there is no third click.
+                            //if is the first click, just count the clicks and open one card.
+                            //before the first click reset the spelling.
+                            //Usefull when there is no third click.
                             card_grid.card_index_of_first_click = 0;
                             card_grid.card_index_of_second_click = 0;
-                            //if is first click, just count the clicks
                             card_grid.card_index_of_first_click = this_click_card_index;
                             card_grid.count_click_inside_one_turn += 1;
                             card_grid.count_all_clicks += 1;
                         } else if card_grid.count_click_inside_one_turn == 1 {
-                            //if is second click, flip the card and then check for card match
+                            //if is the second click, flip the card and then check for card match
                             card_grid.card_index_of_second_click = this_click_card_index;
                             card_grid.count_click_inside_one_turn += 1;
                             card_grid.count_all_clicks += 1;
@@ -206,8 +211,8 @@ impl Render for CardGrid {
             }
         };
 
-        //prepare a vector for the Virtual Dom for grid-item with image
-        //the grid-container needs only grid_items. They are no rows and columns.
+        //prepare a vector for the Virtual Dom for grid_item with <img>
+        //the grid_container needs only grid_items. There is no need for rows and columns in css grid.
         let closure_vec_grid_item_bump = {
             let mut vec_grid_item_bump = Vec::new();
             for x in 1..=16 {
@@ -221,6 +226,7 @@ impl Render for CardGrid {
                         from_card_number_to_img_src(self.vec_cards[index].card_number_and_img_src)
                     }
                 };
+                // code for sound and opacity transition
                 let mut onclick_sound_and_opacity_transition = "";
                 if self.count_click_inside_one_turn <= 1 {
                     onclick_sound_and_opacity_transition = bumpalo::format!(in bump,
@@ -236,9 +242,9 @@ impl Render for CardGrid {
                 if img_src == SRC_FOR_CARD_FACE_DOWN {
                     opacity = bumpalo::format!(in bump, "opacity:{}", 0.2).into_bump_str();
                 }
-
+                //creating 16 <div> in loop
                 let grid_item_bump = div(bump)
-                    .attr("class", "grid-item")
+                    .attr("class", "grid_item")
                     .children([img(bump)
                         .attr("src", img_src)
                         .attr("id", img_id)
@@ -271,39 +277,58 @@ impl Render for CardGrid {
             }
             vec_grid_item_bump
         };
+
+        // the header can show only the game title or two spellings. Not everything together.
+        //game title is visible in the header
+        let mut closure_grid_header = {
+            div(bump)
+                .attr("class", "grid_container_header")
+                .attr("style", "grid-template-columns: auto;")
+                .children([div(bump)
+                    .attr("class", "grid_item")
+                    .attr("style", "text-align: center;")
+                    .children([text(GAME_TITLE)])
+                    .finish()])
+                .finish()
+        };
+        // if the Spellings are visible, than don't show GameTitle, because there is not
+        //enought space on smartphones
+        if self.card_index_of_first_click != 0 || self.card_index_of_second_click == !0 {
+            closure_grid_header = {
+                div(bump)
+                    .attr("class", "grid_container_header")
+                    .attr("style", "grid-template-columns: auto auto;")
+                    .children([
+                        div(bump)
+                            .attr("class", "grid_item")
+                            .attr("style", "text-align: left;")
+                            .children([text(
+                                SPELLING[self.vec_cards[self.card_index_of_first_click]
+                                    .card_number_and_img_src],
+                            )])
+                            .finish(),
+                        div(bump)
+                            .attr("class", "grid_item")
+                            .attr("style", "text-align: right;")
+                            .children([text(
+                                SPELLING[self.vec_cards[self.card_index_of_second_click]
+                                    .card_number_and_img_src],
+                            )])
+                            .finish(),
+                    ])
+                    .finish()
+            };
+        }
         //endregion
 
-        //region: create the virtual dom
+        //region: create the whole virtual dom
         div(bump)
             .attr("class", "m_container")
             .children([
-                //header will have 3 grid items. For spelling and mem_title.
-                div(bump)
-                    .attr("class", "grid-container-header")
-                    .children([
-                        div(bump)
-                            .attr("class", "grid-item")
-                            .attr("style", "text-align: left;")
-                            .children([
-                                text(SPELLING[self.vec_cards[self.card_index_of_first_click].card_number_and_img_src]),
-                            ]).finish(),
-                        div(bump)
-                            .attr("class", "grid-item")
-                            .attr("style", "text-align: center;")
-                            .children([
-                                text(GAME_TITLE)
-                            ]).finish(),
-                        div(bump)
-                            .attr("class", "grid-item")
-                            .attr("style", "text-align: right;")
-                            .children([
-                                text(SPELLING[self.vec_cards[self.card_index_of_second_click].card_number_and_img_src])
-                            ]).finish(),
-                        ])
-                    .finish(),
+                closure_grid_header,
                 //div for the css grid object defined in css with <img> inside
                 div(bump)
-                    .attr("class", "grid-container")
+                    .attr("class", "grid_container")
                     .attr("style", "margin-left: auto;margin-right: auto;")
                     .children(closure_vec_grid_item_bump)
                     .finish(),
